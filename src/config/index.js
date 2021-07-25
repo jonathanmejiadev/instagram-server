81115780bcfabda36e07793750d22afa515c8315
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-07-25 01:00:23 -0300

    Add db config

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,31 @@
-export default {
-    prefix: '/v1',
+require('dotenv').config();
 
-}
+module.exports = {
+    PORT: process.env.PORT || 3001,
+    prefix: '/v1',
+    log: {
+        level: process.env.LOG_LEVEL
+    },
+    db: {
+        postgres: {
+            development: {
+                dbName: process.env.POSTGRES_DB_DEVELOPMENT,
+                user: process.env.POSTGRES_USER,
+                password: process.env.POSTGRES_PASSWORD,
+                host: 'localhost'
+            },
+            test: {
+                dbName: process.env.POSTGRES_DB_TEST,
+                user: process.env.POSTGRES_USER,
+                password: process.env.POSTGRES_PASSWORD,
+                host: 'localhost'
+            },
+            production: {
+                dbName: '',
+                user: '',
+                password: '',
+                host: ''
+            }
+        }
+    },
+};
